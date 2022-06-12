aa14ef76cacd8ba9c052273c566129b4dd2651ed
Syaifuddin1997 <msyaifuddin1997@gmail.com>
2022-06-12 22:31:06 +0700

    tugas 07

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async (emotion) => {
+  try {
+    let sumData =(await promiseTheaterIXX()).concat (await promiseTheaterVGC());
+    return sumData.filter((data) => data.hasil === emotion).length;
+
+  } catch(err) {
+    console.log(err)
+  }
+}
 
 module.exports = {
   promiseOutput,
